558bac09138e05a87e88a7e8c40ff6cc91f60a0c
vicographics <victor.alcocer43@uabc.edu.mx>
2026-04-30 13:26:41 -0700

    initial site setup

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ function irAProyecto(id) {
         behavior: "smooth"
     });
 }
+
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+function showSlide(n) {
+    slides.forEach(s => s.classList.remove("active"));
+    dots.forEach(d => d.classList.remove("active"));
+
+    slideIndex = (n + slides.length) % slides.length;
+
+    slides[slideIndex].classList.add("active");
+    dots[slideIndex].classList.add("active");
+}
+
+function moveSlide(n) {
+    showSlide(slideIndex + n);
+}
+
+function currentSlide(n) {
+    showSlide(n);
+}
+
+// Automático
+setInterval(() => {
+    moveSlide(1);
+}, 8000);
